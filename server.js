1da3e3d11eb1764e977433acd60b19cd5b96a144
MystK <its@phamap.net>
2016-06-04 23:06:01 -0700

    #5: support history tracking on wiki

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import Router from 'koa-router'
 import Reddit from './RedditAPIDriver'
 import i18n from './i18n'
 import { stringify } from 'query-string'
+import fetch from 'node-fetch'
 let locale = 'en-us'
 const dev = true
 let subreddit = 'changemyview'
@@ -76,8 +77,8 @@ const checkForDeltas = async () => {
   try {
     let comments = await getNewComments()
     _.each(comments, (entry, index) => {
-      const { link_title, link_id, author, body, body_html, edited, parent_id, name, author_flair_text, link_url, created_utc, created } = entry.data
-      comments[index] = { link_title, link_id, author, body, body_html, edited, parent_id, name, author_flair_text, link_url, created_utc, created }
+      const { link_title, link_id, author, body, body_html, edited, parent_id, id, name, author_flair_text, link_url, created_utc, created } = entry.data
+      comments[index] = { link_title, link_id, author, body, body_html, edited, parent_id, id, name, author_flair_text, link_url, created_utc, created }
       const removedBodyHTML = body_html.replace(/blockquote&gt;[^]*\/blockquote&gt;/,'').replace(/pre&gt;[^]*\/pre&gt;/,'')
       if (!!removedBodyHTML.match(/&amp;#8710;|&#8710;|∆|Δ|!delta/)) verifyThenAward(comments[index])
     })
@@ -112,13 +113,6 @@ router.get('/checkForDeltas', async (ctx, next) => {
 })
 router.get('/dynamic/*', async (ctx, next) => {
   let response = await reddit.query(`/${ctx.params['0']}?${stringify(ctx.query)}`)
-  if (ctx.params['0'] === `r/${subreddit}/comments?`) {
-    const { children } = response.data
-    _.each(children, (entry, index) => {
-      const { link_title, link_id, author, body, edited, parent_id, name, author_flair_text, link_url, created_utc, created } = entry.data
-      children[index] = { link_title, link_id, author, body, edited, parent_id, name, author_flair_text, link_url, created_utc, created }
-    })
-  }
   ctx.body = response
   await next()
 })
@@ -132,6 +126,7 @@ const bumpFlairCount = async ({ name }) => {
     text: newFlairCount + '∆'
   }
   reddit.query({ URL: `/r/${subreddit}/api/flair?${stringify(flairQuery)}`, method: 'POST' })
+  return newFlairCount
 }
 
 const getFlair = async ({ name }) => {
@@ -146,7 +141,7 @@ const parseHiddenParams = comment => {
   return params
 }
 
-const verifyThenAward = async ({ author, body, link_id: linkID, name, parent_id: parentID }) => {
+const verifyThenAward = async ({ author, body, link_id: linkID, link_title: linkTitle, link_url: linkURL, id, name, parent_id: parentID }) => {
   try {
     console.log( author, body, linkID, parentID )
     const hiddenParams = {
@@ -198,7 +193,8 @@ const verifyThenAward = async ({ author, body, link_id: linkID, name, parent_id:
       text = text.replace(/USERNAME/g, parentThing.author)
       if (query.text.length) query.text += '\n\n'
       query.text += text
-      await bumpFlairCount({ name: author })
+      const flairCount = await bumpFlairCount({ name: parentThing.author })
+      await addDeltaToWiki({ user: parentThing.author, id, linkTitle, linkURL, author, flairCount })
     } else if (issueCount >= 2) {
       let text = i18n[locale].noAward.issueCount
       text = text.replace(/ISSUECOUNT/g, issueCount)
@@ -221,3 +217,67 @@ app
   .listen(80)
 
 setInterval(checkForDeltas, 5000)
+
+const getWikiContent = async user => {
+  try {
+    const resp = await fetch(`https://www.reddit.com/r/${subreddit}/wiki/user/${user}`)
+    const text = await resp.text()
+    return text.match(/<textarea readonly class="source" rows="20" cols="20">[^]+<\/textarea>/)[0].replace(/<textarea readonly class="source" rows="20" cols="20">|<\/textarea>/g, '')
+  } catch (err) {
+    return false
+  }
+}
+
+const addDeltaToWiki = async ({ user, linkTitle, id, linkURL, author, flairCount }) => {
+  let content = await getWikiContent(user)
+  const now = new Date()
+  const [month, day, year] = [now.getMonth() + 1, now.getDate(), now.getFullYear()]
+  const newRow = `|${month}/${day}/${year}|[${linkTitle}](${linkURL})|[Link](${linkURL}${id}?context=2)|/u/${author}|`
+  let query
+  if (content) {
+    query = {
+      page: `user/${user}`,
+      reason: 'Added Delta',
+    }
+    if (content.indexOf(`/u/${user} has received `) === -1) {
+      query.content = `/u/${user} has received ${flairCount} delta(s) for the following comments:\r\n\r\n${content}\r\n\r\n| Date | Submission | Delta Comment | Awarded By |\r\n| --- | :-: | --- | --- |\r\n${newRow}`
+    } else {
+      content = content.replace(
+        /\/u\/\S+ has received \d+ delta[s()]* for the following comments:/,
+        `/u/${user} has received ${flairCount} delta(s) for the following comments:`
+      )
+      if (content.indexOf(`| Date | Submission | Delta Comment | Awarded By |\r\n| --- | :-: | --- | --- |`) > -1) {
+        query.content = content
+        .replace(
+          '| --- | :-: | --- | --- |',
+          `| --- | :-: | --- | --- |\r\n${newRow}`
+        )
+      } else {
+        const pastString = `Any delta history before February 2015 can be found at: /r/ChangeMyView/wiki/userhistory/user/${user}`
+        let lengthToSlice = content.indexOf(content.indexOf(pastString)) + 1
+        if (lengthToSlice > 0) {
+          lengthToSlice += lengthToSlice.length
+          const startContent = content.slice(0, lengthToSlice)
+          const endContent = content.slice(lengthToSlice)
+          query.content = content.replace(
+            `Any delta history before February 2015 can be found at: /r/ChangeMyView/wiki/userhistory/user/${user}`,
+            `Any delta history before February 2015 can be found at: /r/ChangeMyView/wiki/userhistory/user/${user}\r\n\r\n| Date | Submission | Delta Comment | Awarded By |\r\n| --- | :-: | --- | --- |\r\n${newRow}`
+          )
+        } else {
+          query.content = content.replace(
+            `/u/${user} has received ${flairCount} delta(s) for the following comments:`,
+            `/u/${user} has received ${flairCount} delta(s) for the following comments:\r\n\r\n| Date | Submission | Delta Comment | Awarded By |\r\n| --- | :-: | --- | --- |\r\n${newRow}`
+          )
+        }
+      }
+    }
+  } else { // make wiki page
+    query = {
+      page: `user/${user}`,
+      reason: 'Created delta history page with first delta',
+      content: `/u/${user} has received ${flairCount} delta(s) for the following comments:\r\n\r\n| Date | Submission | Delta Comment | Awarded By |\r\n| --- | :-: | --- | --- |\r\n${newRow}`
+    }
+  }
+  await reddit.query({ URL: `/r/${subreddit}/api/wiki/edit?${stringify(query)}`, method: 'POST' })
+  return true
+}
