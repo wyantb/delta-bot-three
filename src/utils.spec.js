e771c611ca842819e19b1ce6ab80737fd1cb2a5f
Antoine Pham <Antoine Pham>
2016-11-11 17:35:15 -0800

    test(utils): add test for checking deltas

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -8,9 +8,22 @@ const componentFileName = fileName.match(/([^]+).spec.js/)[1]
 // bring in the component to be used in tests
 const component = require(`./${componentFileName}.js`)
 
-describe('Transformations for markdown', () => {
+describe('utilities', () => {
   it('should escape only underscores', () => {
     const { escapeUnderscore } = component
     expect(escapeUnderscore('__UsName__')).toBe('\\_\\_UsName\\_\\_')
+  })
+  it('should correctly check for deltas', () => {
+    const { checkCommentForDelta } = component
+    const createMockCommentClass = body_html => ({ body_html })
+    expect(checkCommentForDelta(createMockCommentClass(''))).toBe(false)
+    expect(checkCommentForDelta(createMockCommentClass('!delta'))).toBe(true)
+    expect(checkCommentForDelta(createMockCommentClass('!dElTa'))).toBe(true)
+    expect(checkCommentForDelta(createMockCommentClass('Δ'))).toBe(true)
+    expect(checkCommentForDelta(createMockCommentClass('∆'))).toBe(true)
+    expect(checkCommentForDelta(createMockCommentClass('&#8710;'))).toBe(true)
+    expect(checkCommentForDelta(createMockCommentClass('&amp;#8710;'))).toBe(true)
+    expect(checkCommentForDelta(createMockCommentClass('blockquote&gt;&amp;#8710;&#8710;∆Δ!delta!dElTa/blockquote&gt;'))).toBe(false)
+    expect(checkCommentForDelta(createMockCommentClass('pre&gt;&amp;#8710;&#8710;∆Δ!delta!dElTa/pre&gt;'))).toBe(false)
   })
 })
