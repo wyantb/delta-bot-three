a85087f1a3e73832ca81d2944ce69d58b5e2b99d
MystK <its@phamap.net>
2017-01-10 17:26:48 -0800

    fix(hiddenParam): always html decode hidden params

--- src/parse-hidden-params.js
+++ src/parse-hidden-params.js
@@ -1,10 +1,12 @@
+import { AllHtmlEntities as entities } from 'html-entities'
+
 export default string => {
   try {
     const hiddenSection = string.match(/DB3PARAMSSTART[^]+DB3PARAMSEND/)[0]
     const stringParams = hiddenSection.slice(
       'DB3PARAMSSTART'.length, -'DB3PARAMSEND'.length
     ).replace(/&quot;/g, '"').replace(/-paren---/g, ')')
-    return JSON.parse(stringParams)
+    return JSON.parse(entities.decode(stringParams))
   } catch (error) {
     return false
   }
