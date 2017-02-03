d58f2cfcc7969c5d79ea24c6dc183726090b9a45
RaoulMeyer <raouljmeyer@gmail.com>
2017-02-03 18:25:31 +0100

    fix(doubleDeltaCommets): reapply fix for #28 from pull request #145 (#157)

--- src/index.js
+++ src/index.js
@@ -589,6 +589,20 @@ export const verifyThenAward = async (comment) => {
     link_url: linkURL,
     id,
   } = comment
+
+  // check if DeltaBot has already replied to this comment
+  const commentURL = `${linkURL}${id}.json`.replace('https://www.reddit.com', '')
+  const response = await reddit.query(commentURL, true)
+  const replies = _.get(response, '[1].data.children[0].data.replies')
+  const dbReplied = _.reduce(_.get(replies, 'data.children'), (result, reply) => {
+    if (result) return result
+    return _.get(reply, 'data.author') === botUsername
+  }, false)
+
+  if (dbReplied) {
+    return false
+  }
+
   try {
     const {
       issueCount,
@@ -893,7 +907,6 @@ const checkMessagesforDeltas = async () => {
           const commentLink = comments.commentLinks[i]
           const response = await reddit.query(`${commentLink}`)
           const {
-            replies,
             link_id,
             author,
             body,
@@ -927,22 +940,15 @@ const checkMessagesforDeltas = async () => {
             created_utc,
             created,
           }
-          const dbReplied = _.reduce(_.get(replies, 'data.children'), (result, reply) => {
-            if (result) return result
-            return _.get(reply, 'data.author') === botUsername
-          }, false)
           const removedBodyHTML = (
               body_html
                 .replace(/blockquote&gt;[^]*?\/blockquote&gt;/, '')
                 .replace(/pre&gt;[^]*?\/pre&gt;/, '')
           )
-          if (
-              !dbReplied &&
-              (
-                  !!removedBodyHTML.match(/&amp;#8710;|&#8710;|∆|Δ/) ||
-                  !!removedBodyHTML.match(/!delta/i)
-              )
-          ) await verifyThenAward(comment)
+          if (!!removedBodyHTML.match(/&amp;#8710;|&#8710;|∆|Δ/) ||
+            !!removedBodyHTML.match(/!delta/i)) {
+            await verifyThenAward(comment)
+          }
         }
       } catch (err) {
         console.error(err)
